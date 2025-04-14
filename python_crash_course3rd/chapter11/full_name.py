bc068e3091e304075f7e7a5413ab3d05b671c789
Douglas Alves <rachebartm00s@proton.me>
2025-04-14 22:24:32 +0100

    chapter 11

---
+++ python_crash_course3rd/chapter11/full_name.py
@@ -0,0 +1,3 @@
+def	formatted_name(first_name, middle_name, last_name):
+	full_name = first_name.strip().title() + ' ' + middle_name.strip().title() + ' ' + last_name.strip().title()
+	return full_name
